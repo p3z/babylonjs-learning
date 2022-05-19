a4c53338fa8239ba7a9503ef78f1489a8213665f
Shay <chenille.pottle@yahoo.co.uk>
2022-05-19 08:08:02 +0100

    First render

--- js/main.js
+++ js/main.js
@@ -6,18 +6,27 @@ document.addEventListener("DOMContentLoaded", startGame)
 function startGame(){
        canvas = document.getElementById("renderCanvas");
        engine = new BABYLON.Engine(canvas, true);
-       scene = new BABYLON.Scene(engine);
+       scene = createScene();
        engine.runRenderLoop( () => {
               scene.render();
        });
 }
 
-var createScene = () => {
-       let scene = new BABYLON.Scene(engine);
-       scene.clearColor = new BABYLON.Color3(1,0,1);
-       // Code here
+var createScene = function () {
+       var scene = new BABYLON.Scene(engine);
+       var camera = new BABYLON.ArcRotateCamera("Camera", -Math.PI / 2,  Math.PI / 2, 5, BABYLON.Vector3.Zero(), scene);
+       camera.attachControl(canvas, true);
+          
+       var light = new BABYLON.PointLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+       light.diffuse = new BABYLON.Color3(1, 0, 0);
+       light.specular = new BABYLON.Color3(0, 1, 0);
+
+       var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", {}, scene);
+       sphere.position.z = 1;			
+              
        return scene;
-}
+   
+   };
 
 window.onresize = () => {
        engine.resize();
